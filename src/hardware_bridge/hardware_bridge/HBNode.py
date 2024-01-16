6fca35fd9e65546db01869103d2aab40c33f59a2
Jeffrey Ke <jeffke613@gmail.com>
2024-01-15 20:02:04 -0800

    Started populating HBNode - Created a timer that spins bridge_ at 10Hz - Probably should add a formal callback function for bridge_.Spin(); - I'm not sure how much value there is for multi-threading in Python.

--- src/hardware_bridge/hardware_bridge/HBNode.py
+++ src/hardware_bridge/hardware_bridge/HBNode.py
@@ -2,19 +2,22 @@ import rclpy
 from rclpy.node import Node
 from HardwareBridge import HardwareBridge
 
+BRIDGE_SPIN_FREQUENCY = 10 # Hz
+
+
 class HBNode(Node):
 
     bridge_ = HardwareBridge()
 
-    def __init__():
+    def __init__(self):
         super().__init__("HBNode")
         
 
         # timer to spin bridge
-
+        self.bridge_timer_ = self.create_timer(1/BRIDGE_SPIN_FREQUENCY, lambda _: self.bridge_.Spin())
 
         # timer to read from bridge and publish
-
+        
         # subscribers that feed the bridge
 
         
