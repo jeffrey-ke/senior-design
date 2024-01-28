2579794001bba5d9a69064f53109549a98991885
jeff <jke@scu.edu>
2024-01-28 22:49:39 +0000

    Bug squash

--- src/hardware_bridge/hardware_bridge/HBNode.py
+++ src/hardware_bridge/hardware_bridge/HBNode.py
@@ -2,8 +2,8 @@ import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-from HardwareBridge import HardwareBridge
-from MessageCreator import MessageCreator
+from .HardwareBridge import HardwareBridge
+from .MessageCreator import MessageCreator
 from geometry_msgs.msg import Point, Quaternion
 
 BRIDGE_SPIN_FREQUENCY = 10 # Hz, make this a ros2 param
@@ -14,8 +14,7 @@ MSG_CR_READ_FREQUENCY = 10
 
 class HBNode(Node):
 
-    bridge_ = HardwareBridge("ACM0", 115200, 1) #Add port and baud and timeout
-    msg_creator_ = MessageCreator()
+    
 
     bridge_callback_g_ = ReentrantCallbackGroup()
     msg_cr_callback_g_ = ReentrantCallbackGroup()
@@ -23,10 +22,15 @@ class HBNode(Node):
 
     def __init__(self):
         super().__init__("HBNode")
+
         
+        if (self.InitAll() is False):
+            self.get_logger().info("INIT FAILED! SHUTTING DOWN")
+            self.Shutdown()
+    
         # timer to spin bridge
         self.bridge_spin_timer_ = self.create_timer(1/BRIDGE_SPIN_FREQUENCY, 
-                                                    lambda _: self.bridge_.Spin(), 
+                                                    self.BridgeSpin, 
                                                     callback_group=self.bridge_callback_g_)
 
         # timer to read from bridge and publish
@@ -35,24 +39,51 @@ class HBNode(Node):
                                                     callback_group=self.bridge_callback_g_)
         
         self.msg_cr_spin_timer_ = self.create_timer(1/MSG_CR_SPIN_FREQUENCY,
-                                                    lambda _: self.msg_creator_.Spin(),
+                                                    self.MsgCrSpin,
                                                     callback_group=self.msg_cr_callback_g_)
+         
         self.msg_cr_read_timer_ = self.create_timer(1/MSG_CR_READ_FREQUENCY,
-                                                    self.ReadFromMessageCreatorAndPublish(), 
+                                                    self.ReadFromMessageCreatorAndPublish, 
                                                     callback_group=self.msg_cr_callback_g_)
         
         self.raw_GPS_pub_ = self.create_publisher(Point, "/raw_gps", 10)
         self.raw_IMU_pub_ = self.create_publisher(Quaternion, "/raw_imu", 10)
         # subscribers that feed the bridge
 
+    def BridgeSpin(self):
+        self.get_logger().info("\tBridge spinning..")
+        self.bridge_.Spin()
+
+    def MsgCrSpin(self):
+        self.get_logger().info("\tMsg Cr Spinning..")
+        self.msg_creator_.Spin()
+
+    def Shutdown(self):
+        rclpy.shutdown()
+
+    def InitAll(self) -> bool:
+        bridge_init = self.InitBridge()
+        msg_cr_init = self.InitMsgCr()
+        print("Bridge init: " + str(bridge_init))
+        print("Message Creator init: " + str(msg_cr_init))
+
+        return (bridge_init is True) and (msg_cr_init is True)
+
+
+    def InitBridge(self) -> bool:
+        self.bridge_ = HardwareBridge("ACM0", 115200, 1) #Add port and baud and timeout
+        return self.bridge_.init_successful_
+
+    def InitMsgCr(self) -> bool:
+        self.msg_creator_ = MessageCreator()
+        return True
         
     def BridgeReadCb(self):
         raw = self.bridge_.Read()
         self.msg_creator_.EnqueueRaw(raw)
         self.get_logger().info("msg: " + str(raw))
         
-
-        # Publishers
+    # Publishers
     def ReadFromMessageCreatorAndPublish(self):
         msg = self.msg_creator_.Read()
         if (msg is None):
@@ -65,7 +96,7 @@ class HBNode(Node):
 def main(args=None):
     rclpy.init()
     executor = MultiThreadedExecutor()
-    executor.add_node(HBNode)
+    executor.add_node(HBNode())
     executor.spin()
     rclpy.shutdown()
 
