40e157395c317dccc0bda6a46fee4e63d7cca97a
jeff <jke@scu.edu>
2024-02-01 21:56:12 +0000

    using lat long for waypoint control

--- src/velocity_commander/velocity_commander/NavigationNode.py
+++ src/velocity_commander/velocity_commander/NavigationNode.py
@@ -1,33 +1,50 @@
 
-from ast import Constant
-from tokenize import Double
 from typing import Tuple
-import math
-PI = 3.141592654
+from math import sin, cos, sqrt, radians, atan2, pi
 K_l = 0.5
 K_a = 0.5
 distanceTolerance = 0.1
 angleTolerance = 0.1 
+
+
+def getHeadingError(lat1, long1, lat2, long2, current_heading):
+  y = sin(long2 - long1) * sin(lat2) 
+  x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(long2 - long1)
+  bearing = atan2(y, x)
+  h_err = bearing - current_heading
+
+  if (h_err > pi):
+    h_err = h_err - (2 * pi)  
+  if (h_err < -pi):
+    h_err = h_err + (2 * pi)
+
+  return h_err
+
+def getDistanceToWaypoint(lat1, long1, lat2, long2) -> float:
+  RADIUS_EARTH = 6378.1370e3
+  lat1 = radians(lat1)
+  long1 = radians(long1)  
+  lat2 = radians(lat2)
+  long2 = radians(long2)
+
+  delta_lat = lat2 - lat1
+  delta_long = long2 - long1
+
+  a = sin(delta_lat/2)**2 + cos(lat1) * cos(lat2) * sin(delta_long/2)**2
+  c = 2 * atan2(sqrt(a), sqrt(1 - a))
+  return RADIUS_EARTH * c
+
 #takes in the current point and goal point as x,y,z as well as heading and returns the linear and angular velocity as (lx,ly,lz,ax,ay,az)
-def waypointToVelocity(currentPoint: Tuple[float, float, float], goalPoint: Tuple[float, float, float], heading: float ) -> Tuple[float, float, float, float, float, float]: 
+def waypointToVelocity(lat1, long1, lat2, long2, current_heading) -> Tuple[float, float, float, float, float, float]: 
 #Get the heading error i.e. how many radians does the robot need to turn to head towards the waypoint 
-  deltaX = goalPoint[0] - currentPoint[0]
-  deltaY = goalPoint[1] - currentPoint[1]
-  waypointHeading = math.atan2(deltaY, deltaX)
-  headingError = waypointHeading - heading;   
-   
-  #Make sure heading error falls within -PI to PI range
-  if (headingError > PI):
-    headingError = headingError - (2 * PI)
-  if (headingError < -PI):
-    headingError = headingError + (2 * PI)
+  heading_error = getHeadingError(radians(lat1), radians(long1), radians(lat2), radians(long2), current_heading)  
  
-  distanceToWaypoint = getDistanceToWaypoint(currentPoint,goalPoint)
+  distanceToWaypoint = getDistanceToWaypoint(lat1, long1, lat2, long2)
 
   if (abs(distanceToWaypoint) > distanceTolerance):    
     # If the robot's heading is off, fix it.
-    if (abs(headingError) > angleTolerance):
-      return (0.0,0.0,0.0,0.0,0.0,K_a  * headingError)
+    if (abs(heading_error) > angleTolerance):
+      return (0.0,0.0,0.0,0.0,0.0,K_a  * heading_error)
     #Just fix the distance gap between current pose and waypoint. The magnitude of the robot's velocity 
     #is directly proportional to the distance the robot is from the goal.
     else:
@@ -35,9 +52,7 @@ def waypointToVelocity(currentPoint: Tuple[float, float, float], goalPoint: Tupl
   else:
     return (0.0,0.0,0.0,0.0,0.0,0.0)
 
-def getDistanceToWaypoint(currentPoint: Tuple[float, float, float], goalPoint: Tuple[float, float, float]) -> float:
-  return math.sqrt(pow(goalPoint[0] - currentPoint[0], 2) + pow(
-    goalPoint[0] - currentPoint[0], 2))
+
 
 #Takes in desired velocity as (lx,ly,lz,ax,ay,az) and returns PMU commands for thruster as (FL,FR,DL,DR)
 def velocityToPWM(vel: Tuple[float, float, float, float, float, float]) -> Tuple[float, float, float, float]:
