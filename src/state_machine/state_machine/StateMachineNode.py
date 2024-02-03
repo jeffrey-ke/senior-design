353937568e696c77671720812700cdc5fa1703e5
Ricky Schober <rschober@scu.edu>
2024-02-03 15:44:27 -0800

    started state machine

--- src/state_machine/state_machine/StateMachineNode.py
+++ src/state_machine/state_machine/StateMachineNode.py
@@ -1,23 +1,38 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 from time import time
 from geographic_msgs.msg import GeoPoint
 from std_msgs.msg import Int16
+from std_msgs.msg import String
+from Waypoint.action import Waypoint
+
 
 MAX_DURATION_SECONDS = 600
 SPIN_FREQUENCY = 100
 
 lat = 0.0
 lon = 0.0
-
+waypoints = [] #stack of waypoints push new ones to back pop old ones from front
 class StateMachineNode(Node):
     def __init__(self):
-
+        super().__init__('StateMachineNode')
         ###################
         # Publishers ######
         ###################
-        self.wp_pub_ = self.create_publisher(GeoPoint, "/waypoint", 10)
         self.kill_pub_ = self.create_publisher(Int16, "/KILL", 10)
+        self.state_pub_ = self.create_publisher(String, "State", 10)
+
+        ##################
+        # Subscribers ####
+        ##################
+        self.command_sub_ = self.create_subscription(String,"/Command",self.command_callback,10)
+        self.subscription  # prevent unused variable warning
+
+        ##################
+        # Actions ########
+        ##################
+        self.waypoint_client_ = ActionClient(self, Waypoint, 'waypoint')
 
         ###################
         # Timers
@@ -26,6 +41,42 @@ class StateMachineNode(Node):
 
         self.start_time_ = time()
         self.cur_time_ = self.start_time_
+
+    #Parse command from base station 
+    def command_callback(self, msg):
+        splitmsg = msg.split(':')
+        if(splitmsg[0]=="W"):
+            splitcoords = splitmsg.split(',')
+            waypoints.append(splitcoords[0],splitcoords[1])
+        elif(splitmsg[0]=="K"):
+            self.kill_pub_.publish(1) #kill command
+
+    def send_waypoint(self, coords):
+        goal_msg = Waypoint.Goal()
+        goal_msg.waypointCoords = coords
+
+        self.waypoint_client_.wait_for_server()
+
+        self.waypoint_result = self.waypoint_client_.send_goal_async(goal_msg)
+
+        self.waypoint_result.add_done_callback(self.waypoint_goal_response)
+
+    def waypoint_goal_response(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self.waypoint_result_future = goal_handle.get_result_async()
+        self.waypoint_result_future.add_done_callback(self.waypoint_result_callback)
+
+    def waypoint_result_callback(self, future):
+        result = future.result().result
+        if(result):
+            waypoints.pop(0)
+            
 
     def Spin(self):
         self.wp_pub_.publish(GeoPoint())
