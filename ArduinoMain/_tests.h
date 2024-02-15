9a9173afc63057fd060227d596d6544347582064
Jeffrey Ke <jeffke613@gmail.com>
2024-02-15 13:56:26 -0800

    expanding tests

--- ArduinoMain/_tests.h
+++ ArduinoMain/_tests.h
@@ -5,14 +5,32 @@
 
 test(basic_controller_test){
     Controller c(1, 0, 0);
+    assertEqual(c.GetKp(), 1);
+
     c.SetDesired(10);
+    assertEqual(c.GetDesired(), 10);
+
     auto eff = c.CalculateControlEffort(0);
     assertEqual(eff, 10 * c.GetKp());
 
+    eff = c.CalculateControlEffort(5);
+    assertEqual(eff, 5 * c.GetKp());
+
     eff = c.CalculateControlEffort(10);
     assertEqual(eff, 0);
+
+    c.SetDesired(0);
+    assertEqual(c.GetDesired(), 0);
+
+    eff = c.CalculateControlEffort(10);
+    assertEqual(eff, -10 * c.GetKp());
 }
 
+test(wp_controller) {
+    WaypointController wp_c(1, 0, 0, 0
+                            1, 0, 0, 0);
+    wp_c.UpdateDesiredLocation()
+}
 
 
 void setup() {
