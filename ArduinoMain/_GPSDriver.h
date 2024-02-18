c80114ee1531e79d9c79ed828ba3fecf9ca87a9a
Jeffrey Ke <jeffke613@gmail.com>
2024-02-17 19:42:32 -0800

    update

--- ArduinoMain/_GPSDriver.h
+++ ArduinoMain/_GPSDriver.h
@@ -4,10 +4,18 @@
 #include "Constants.h"
 
 class _GPSDriver{
+public:
+  constexpr static double INVALID = -99.0;
+
+  _GPSDriver();
+  void Refresh();
+  double GetLat() const {return (fix_)? lat_: INVALID;}
+  double GetLong() const {return (fix_)? long_: INVALID;}
+  double GetHeading() const {return (fix_) ? heading_: INVALID;}
 private:
   Adafruit_GPS GPS;
-public:
-  _GPSDriver();
-  double* getData();
+  double lat_{INVALID}, long_{INVALID}, heading_{INVALID};
+  bool fix_{false};
+  uint32_t timer_;
 };
 #endif
