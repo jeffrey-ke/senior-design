11e1956c7d8f2c4ebee0354cc95900f17c7e3e9d
Jeffrey Ke <jeffke613@gmail.com>
2024-02-17 18:16:47 -0800

    call by reference

--- ArduinoMain/WaypointController.cpp
+++ ArduinoMain/WaypointController.cpp
@@ -3,7 +3,7 @@
 double WaypointController::LINEAR_MARGIN_OF_ERROR = 1.0; //meters
 double WaypointController::ANGULAR_MARGIN_OF_ERROR = 0.1; //radians
 
-WaypointController::PWMCommand WaypointController::CalculatePWM(Location current_loc) {
+WaypointController::PWMCommand WaypointController::CalculatePWM(const Location& current_loc) {
     auto l_pwm = CalculateLinearPWM(current_loc);
     auto a_pwm = CalculateAngularPWM(current_loc);
 
@@ -14,22 +14,22 @@ WaypointController::PWMCommand WaypointController::CalculatePWM(Location current
     }
 }
 
-WaypointController::PWMCommand WaypointController::CalculateLinearPWM(Location current_loc) {
+WaypointController::PWMCommand WaypointController::CalculateLinearPWM(const Location& current_loc) {
     auto current_distance = CalculateDistanceBetween(current_loc, desired_);
     auto linear_effort_per_thruster =  static_cast<int>(linear_controller_.CalculateControlEffort(current_distance) / 2);
     return PWMCommand{1500 + linear_effort_per_thruster, 1500 + linear_effort_per_thruster, 1500, 1500};
 }
 
-WaypointController::PWMCommand WaypointController::CalculateAngularPWM(Location current_loc) {
+WaypointController::PWMCommand WaypointController::CalculateAngularPWM(const Location& current_loc) {
     auto current_angular_difference = current_loc.heading - CalculateAngleDifferenceBetween(desired_, current_loc);
     auto angular_effort_per_thruster = static_cast<int>(angular_controller_.CalculateControlEffort(current_angular_difference) / 2);
     return PWMCommand{1500 - angular_effort_per_thruster, 1500 + angular_effort_per_thruster, 1500, 1500};
 }
 
-bool WaypointController::IsHeadingCorrectWithinMargin(Location current_loc) {
+bool WaypointController::IsHeadingCorrectWithinMargin(const Location& current_loc) {
     return current_loc.heading - CalculateAngleDifferenceBetween(current_loc, desired_) < ANGULAR_MARGIN_OF_ERROR;
 }
 
-bool WaypointController::IsLocationCorrectWithinMargin(Location current_loc) {
+bool WaypointController::IsLocationCorrectWithinMargin(const Location& current_loc) {
     return CalculateDistanceBetween(current_loc, desired_) < LINEAR_MARGIN_OF_ERROR;
 }
