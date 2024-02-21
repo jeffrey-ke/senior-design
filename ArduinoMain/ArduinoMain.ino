754bac6ca6f239709fa1752edc59be81715249ea
Jeffrey Ke <jeffke613@gmail.com>
2024-02-21 14:43:05 -0800

    tentative hardware tests

--- ArduinoMain/ArduinoMain.ino
+++ ArduinoMain/ArduinoMain.ino
@@ -6,26 +6,32 @@
 #include "MasterComputeBridge.h"
 #include "Constants.h"
 #include "WaypointController.h"
+#include "StateMachine.h"
 
 
-  void setup() {
-    // put your setup code here, to run once:
-    PISerial.begin(115200);
-    MasterComputeBridge bridge; 
-    //bridge = MasterComputeBridge();
-    DebugSerial.println("Initialization Done");
-    
-    while(true){
-      bridge.giveCommand("G:");
-      PISerial.println(bridge.returnCommand());
-      delay(1000);
-    }
-  }
+StateMachine sm(1, 0, 0, 0,
+                1, 0, 0, 0,
+                5);
+void setup() {
+  // put your setup code here, to run once:
+  PISerial.begin(115200);
+  
+}
 
-  void loop() {
-    // put your main code here, to run repeatedly:
-    
-  }
+void loop() {
+  // put your main code here, to run repeatedly:
+    sm.DecideState();
+    sm.ExecuteState();
+    auto pwm = sm.GetCommandedPWM();
+    PISerial.print("FL: ");
+    PISerial.println(pwm.FL);
+    PISerial.print("FR: ");
+    PISerial.println(pwm.FR);
+    PISerial.print("DL: ");
+    PISerial.println(pwm.DL);
+    PISerial.print("DR: ");
+    PISerial.println(pwm.DR);
+}
 #elif IS_TEST
 #include "_tests.h"
 #include "_StateMachineTests.h"
