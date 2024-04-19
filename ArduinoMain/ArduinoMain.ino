34de4f6f43c4bd37efdafb16ccdb0cc0df4b3091
Jeffrey Ke <jeffke613@gmail.com>
2024-04-19 15:32:58 -0700

    gps needs to be spun before coordinates are read

--- ArduinoMain/ArduinoMain.ino
+++ ArduinoMain/ArduinoMain.ino
@@ -84,6 +84,7 @@ void loop() {
 
 void WaypointTest(milliseconds duration, double kp_a, double ki_a, double kd_a, 
                     meters distance_threshold, degrees heading_threshold, Msg::GNSS goal_point) {
+    gps_.Refresh();
     Msg::GNSS home_coords = gps_.GetGNSS();
     o_con_.SetGains(kp_a, ki_a, kd_a, OrientationController::YAW);
     Timer timer_(duration);
